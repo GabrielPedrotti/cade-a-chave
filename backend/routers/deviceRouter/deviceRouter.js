d08ce881c2a3a997024c022c389bc34b62480e41
marcofeliponi <marco.feliponi@outlook.com>
2023-11-23 20:28:34 -0300

    feat: routes

--- backend/routers/deviceRouter/deviceRouter.js
+++ backend/routers/deviceRouter/deviceRouter.js
@@ -4,8 +4,8 @@ const deviceService = require('../../services/deviceService/deviceService');
 const router = express.Router();
 
 router.post('/', deviceService.createDevice);
-router.get('device/:deviceId', deviceService.getDeviceById);s
-router.get('user/:userId', deviceService.getDevicesByUserId);
+router.get('deviceId/:deviceId', deviceService.getDeviceById); s
+router.get('userId/:userId', deviceService.getDevicesByUserId);
 router.put('/:deviceId/:ring', deviceService.updateDeviceAlarm);
 
 module.exports = router;
