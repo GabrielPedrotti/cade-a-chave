f6b1ebe9b7dfed9012f2ad43771bc5c1733083a7
marcofeliponi <marco.feliponi@outlook.com>
2023-11-23 20:37:17 -0300

    fix: routes

--- backend/routers/deviceRouter/deviceRouter.js
+++ backend/routers/deviceRouter/deviceRouter.js
@@ -4,8 +4,8 @@ const deviceService = require('../../services/deviceService/deviceService');
 const router = express.Router();
 
 router.post('/', deviceService.createDevice);
-router.get('deviceId/:deviceId', deviceService.getDeviceById);
-router.get('userId/:userId', deviceService.getDevicesByUserId);
+router.get('/deviceId/:deviceId', deviceService.getDeviceById);
+router.get('/userId/:userId', deviceService.getDevicesByUserId);
 router.put('/:deviceId/:ring', deviceService.updateDeviceAlarm);
 
 module.exports = router;
